da7ddef810f948852016543b4331c5dbaff66e43
Annie J. Wang <anniejw6@gmail.com>
2016-05-03 08:19:00 -0400

    add code template

---
+++ satt_func.R
@@ -0,0 +1,24 @@
+match_samp <- function(x, treat, y){
+
+	# x should be a numeric matrix
+	# treat should be a numeric vector
+	# y should be a numeric vector
+
+
+	# run lasso to find covariates that predict x
+
+	# run genmatch
+
+	# run gam to estimate
+
+
+	# return matched sample
+
+}
+
+satt_est <- function(matched_sample){
+
+	# run gam here ... 
+
+	# return satt, confidence interval
+}
